de5d40e9719168c7bd3a381d8d0cddd3f763834b
cbetz1 <cbetz@svsu.edu>
2017-09-25 16:01:00 -0400

    2nd commit

--- app/src/main/java/com/example/chad/program1/MainActivity.java
+++ app/src/main/java/com/example/chad/program1/MainActivity.java
@@ -23,8 +23,11 @@ public class MainActivity extends AppCompatActivity {
         EditText degreeView = (EditText)findViewById(R.id.degree);
         EditText majorView = (EditText)findViewById(R.id.major);
         EditText favActivitiesView = (EditText)findViewById(R.id.fav_activities);
-        String messageText = "Hello my name is " + firstNameView.getText().toString() + " " + lastNameView.getText().toString() + ". I attend(ed) " + schoolView.getText().toString() + " and graduate(d) in " + yearGradView.getText().toString() + " with a "
-                 + degreeView.getText().toString().toUpperCase() + " degree in " + majorView.getText().toString() + ". I enjoy " + favActivitiesView.getText().toString() + ".";
+        String messageText = "Hello my name is " + firstNameView.getText().toString() + " " +
+                lastNameView.getText().toString() + ". I attend(ed) " + schoolView.getText().toString()
+                + " and graduate(d) in " + yearGradView.getText().toString() + " with a " +
+                degreeView.getText().toString().toUpperCase() + " degree in " + majorView.getText().toString()
+                + ". I enjoy " + favActivitiesView.getText().toString().toLowerCase() + ".";
         Intent intent = new Intent(this, CreateBioActivity.class);
         intent.putExtra("message", messageText);
         startActivity(intent);
